a44019e3d0a0fb3b18e4a9185a10a32d7717a806
ttovarra123 <71203987+ttovarra123@users.noreply.github.com>
2024-10-26 10:48:58 -0600

    Cambio 1 Tovar
    Confirmación ok Tovar 1

---
+++ proyectoistovar/src/proyectoistovar/Proyectoistovar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoistovar;
+
+/**
+ *
+ * @author Beto
+ */
+public class Proyectoistovar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba de repositorio 1 IS Tovar 16/10/2024");
+        System.out.println("Fase de desarrollo - Repositorio en la nube");
+    }
+    
+}
