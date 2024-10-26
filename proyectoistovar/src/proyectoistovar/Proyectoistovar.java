6f620ce33dd0d6d31e773c97721445b5050e03d1
ttovarra123 <71203987+ttovarra123@users.noreply.github.com>
2024-10-26 10:53:30 -0600

    Cambio 2 OK
    Confirmación de cambio 2 Tovar

--- proyectoistovar/src/proyectoistovar/Proyectoistovar.java
+++ proyectoistovar/src/proyectoistovar/Proyectoistovar.java
@@ -17,6 +17,14 @@ public class Proyectoistovar {
     public static void main(String[] args) {
         System.out.println("Prueba de repositorio 1 IS Tovar 16/10/2024");
         System.out.println("Fase de desarrollo - Repositorio en la nube");
+        
+        int num1=10;
+        int num2=20;
+        int suma = num1+num2;
+        System.out.println("Suma es : "+suma);
+        
+        
+        
     }
     
 }
